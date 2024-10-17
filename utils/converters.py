d32cff7d7117a09d57baa1942230f88d648637da
tigranfah <tigran.fahradyan01@gmail.com>
2024-10-17 18:43:18 +0400

    add cif to rdkit mol converted

--- utils/converters.py
+++ utils/converters.py
@@ -1,5 +1,7 @@
-import openbabel
+from openbabel import openbabel
 from pathlib import Path
+from rdkit import Chem
+import tempfile
 
 
 def convert_xyz_to_mol(xyz_file: Path | str, mol_file: Path | str):
@@ -8,3 +10,16 @@ def convert_xyz_to_mol(xyz_file: Path | str, mol_file: Path | str):
     mol = openbabel.OBMol()
     conv.ReadFile(mol, str(xyz_file))
     conv.WriteFile(mol, str(mol_file))
+
+
+def cif_to_mol(file_path: str):
+    obConversion = openbabel.OBConversion()
+    obConversion.SetInAndOutFormats("cif", "mol")
+
+    mol = openbabel.OBMol()
+    obConversion.ReadFile(mol, file_path)
+    with tempfile.NamedTemporaryFile() as tmp:
+        obConversion.WriteFile(mol, tmp.name)
+        mol = Chem.MolFromMolFile(tmp.name)
+
+    return mol
