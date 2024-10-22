85c709b320abdbbaa3de2bd517701b75316936bf
progerdav <developer.gyulnazaryan@gmail.com>
2024-10-22 11:26:16 +0400

    Add basic converter tests

--- utils/converters.py
+++ utils/converters.py
@@ -1,25 +1,40 @@
+import tempfile
+
 from openbabel import openbabel
 from pathlib import Path
 from rdkit import Chem
-import tempfile
 
 
-def convert_xyz_to_mol(xyz_file: Path | str, mol_file: Path | str):
-    conv = openbabel.OBConversion()
-    conv.SetInAndOutFormats("xyz", "mol")
-    mol = openbabel.OBMol()
-    conv.ReadFile(mol, str(xyz_file))
-    conv.WriteFile(mol, str(mol_file))
+def obabel_convert(file_path: Path | str, obConversion: openbabel.OBConversion, canonicalize: bool = False):
+    obMol = openbabel.OBMol()
+    obConversion.ReadFile(obMol, file_path)
+    with tempfile.NamedTemporaryFile() as tmp:
+        obConversion.WriteFile(obMol, tmp.name)
+        mol = Chem.MolFromMolFile(tmp.name, removeHs=False)
+        if canonicalize:
+            mol = Chem.Mol(mol)
+            Chem.SanitizeMol(mol)
+
+        # If molecule has no atoms, error out
+        if mol is None or mol.GetNumAtoms() == 0:
+            raise ValueError(f"Failed to convert {file_path} to mol.")
+
+        # If there are no conformers, error out
+        if mol.GetNumConformers() == 0 or not obMol.Has3D():
+            raise ValueError(f"Failed to convert {file_path} to mol.")
+
+    return mol
 
 
-def cif_to_mol(file_path: str):
+def convert_xyz_to_mol(file_path: Path | str, canonicalize: bool = False):
+    obConversion = openbabel.OBConversion()
+    obConversion.SetInAndOutFormats("xyz", "mol")
+
+    return obabel_convert(file_path, obConversion, canonicalize)
+
+
+def convert_cif_to_mol(file_path: str, canonicalize: bool = False):
     obConversion = openbabel.OBConversion()
     obConversion.SetInAndOutFormats("cif", "mol")
 
-    mol = openbabel.OBMol()
-    obConversion.ReadFile(mol, file_path)
-    with tempfile.NamedTemporaryFile() as tmp:
-        obConversion.WriteFile(mol, tmp.name)
-        mol = Chem.MolFromMolFile(tmp.name)
-
-    return mol
+    return obabel_convert(file_path, obConversion, canonicalize)
