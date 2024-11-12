9e857bb48dfc8ecb234373a919df0c7d70e6c5d4
progerdav <developer.gyulnazaryan@gmail.com>
2024-11-12 15:30:31 +0400

    Finalize converter tests

--- tests/test_converters.py
+++ tests/test_converters.py
@@ -1,14 +1,9 @@
-import os
 import pytest
-import numpy as np
 
-from tqdm import tqdm
 from rdkit import Chem
-from rdkit.Chem import rdMolAlign 
 
 from utils.converters import convert_cif_to_mol, convert_xyz_to_mol
 from tests.fixtures.molecules import CANONICAL_ASPIRIN_SMILES
-from tests.utils.conversion_validators import validate_xyz_conversion
 
 
 def test_basic_convert_cif_to_mol(cif_aspirin_molecule):
@@ -54,26 +49,3 @@ def test_conformer_coordinates(xyz_aspirin_molecule, cif_aspirin_molecule):
 def test_basic_conversion_exception():
     with pytest.raises(ValueError):
         convert_cif_to_mol("non_existent_file.cif")
-
-@pytest.mark.skip(reason="This test is in progress")
-def test_qm9_conversion():
-    QM9_PATH = "./fixtures/dsgdb9nsd_000087.xyz"
-    # List all files ending with .xyz
-    xyz_files = [f for f in os.listdir(QM9_PATH) if f.endswith(".xyz") and "gdb" in f]
-    inv = 0
-    with_dot = 0
-    for f in tqdm(xyz_files):
-        try:
-            mol = convert_xyz_to_mol(os.path.join(QM9_PATH, f))
-        except:
-            inv += 1
-            continue
-
-        assert mol is not None
-        smi = Chem.MolToSmiles(mol)
-        assert smi != ""
-        with_dot += "." in smi
-
-        inv += validate_xyz_conversion(os.path.join(QM9_PATH, f), mol)
-
-    assert [inv, with_dot] == [0, 0]
