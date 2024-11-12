0e1a14896855d1e6fb2d5290fb1fa7f0e5f678f1
progerdav <developer.gyulnazaryan@gmail.com>
2024-11-12 16:01:27 +0400

    Integrate ICM Py and add test

---
+++ tests/test_icmpy.py
@@ -0,0 +1,24 @@
+import os
+
+from icmpy.ConfGenerator import ConfGenerator
+ICM_PATH = os.environ.get("ICM_PATH", "/auto/home/menuab/icm-3.9-4/icm") 
+
+def test_basic_icmpy_conformer_generation():
+    mol_identifiers = [
+        "CC(=O)OC1=CC=CC=C1C(=O)O", # Aspirin
+        "CCCCC",
+    ]  # This can also include paths to sdf files
+
+    config_file = "icmpy/default_config.yaml"
+
+    generator = ConfGenerator(config_file=config_file, icm_path=ICM_PATH)
+    results = generator.process_mol_list(
+        mol_identifiers, n_jobs=1, show_progress=True
+    )
+
+    assert len(results) == 2
+    assert all([r.success for r in results])
+    assert all([r.mol.GetNumConformers() for r in results])
+    assert results[0].smiles == "CC(=O)OC1=CC=CC=C1C(=O)O"
+    assert results[1].smiles == "CCCCC"
+
