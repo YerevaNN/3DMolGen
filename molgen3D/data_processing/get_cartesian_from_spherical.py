8577be9313636f0be7c28ccc520e9229cd72033f
Filya Geikyan <filya.geikyan@gmail.com>
2025-03-04 16:01:53 +0400

    spherical

---
+++ molgen3D/data_processing/get_cartesian_from_spherical.py
@@ -0,0 +1,288 @@
+import numpy as np
+from rdkit import Chem
+import re
+import sys
+import json
+import numpy as np
+
+exclude_h = False
+
+def parse_embedded_smiles(embedded_smiles, embedding_type="spherical"):
+    pattern = r'([A-Za-z][a-z]?)(<([^>]+)>)?'
+    tokens = re.findall(pattern, embedded_smiles)
+    
+    descriptors = []
+    
+    for token in tokens:
+        atom = token[0]
+        descriptor_str = token[2]
+        if descriptor_str:
+            try:
+                descriptor = [float(x) for x in descriptor_str.split(',')]
+                if embedding_type == "spherical" and len(descriptor) != 4:
+                    raise ValueError
+                elif embedding_type == "cartesian" and len(descriptor) != 3:
+                    raise ValueError
+                descriptors.append(descriptor)
+            except:
+                raise ValueError(f"Invalid descriptor format for atom {atom}: '{descriptor_str}'")
+    
+    smiles = re.sub(r'<[^>]+>', '', embedded_smiles) # exclude <...>s
+    return smiles, descriptors
+
+def relative(r, theta, phi):
+    x = r * np.sin(theta) * np.cos(phi)
+    y = r * np.sin(theta) * np.sin(phi)
+    z = r * np.cos(theta)
+    return np.array([x, y, z])
+
+def is_collinear(p1, p2, p3, tolerance=1e-6):
+    v1 = p2 - p1
+    v2 = p3 - p1
+    cross_product = np.cross(v1, v2)
+    return np.allclose(cross_product, [0, 0, 0], atol=tolerance)
+
+def calc_spherical_to_cartesian(r, theta, phi, sign_phi, p_f, p_c1, p_c2):
+    v1 = p_c1 - p_f
+    v2 = p_c2 - p_f
+    
+    # Orthonormal basis
+    e1 = v1 / np.linalg.norm(v1)
+    e3 = np.cross(v1, v2)
+    e3 /= np.linalg.norm(e3) # norm_normal is not 0, bcs we excluded that(when the ref points are collinear) case from data
+    e2 = np.cross(e3, e1)
+    e2 /= np.linalg.norm(e2)
+
+    # Create rotation matrix from local to global coordinates
+    rotation_matrix = np.vstack([e1, e2, e3]).T  # 3x3 matrix
+    
+    # Convert spherical to Cartesian in local coordinates
+    actual_phi = sign_phi * phi
+    local_relative_pos = relative(r, theta, actual_phi)
+
+    # Rotate to global coordinates
+    global_relative_pos = rotation_matrix @ local_relative_pos
+    
+    # Assign global position
+    pos = p_f + global_relative_pos
+    return pos
+
+def assign_cartesian_coordinates(mol, descriptors):
+    """Returns the molecule with assigned 3D coordinates."""  
+    def find_next_atom(begin_smiles_id, smiles_id1, smiles_id2 = -1): #smiles ids same as sdf ids
+        this_atom1 = mol.GetAtomWithIdx(smiles_id1)
+        neighbors1 = [neighbor.GetIdx() for neighbor in this_atom1.GetNeighbors()]
+        neighbors2 = []
+        if smiles_id2 != -1:
+            this_atom2 = mol.GetAtomWithIdx(smiles_id2)
+            neighbors2 = [neighbor.GetIdx() for neighbor in this_atom2.GetNeighbors()]
+        for i in range(begin_smiles_id - 1, -1, -1):
+            if i == smiles_id1 or i == smiles_id2:
+                continue
+            if i in neighbors1 or i in neighbors2:
+                return i
+        return -1
+    
+    conf = Chem.Conformer(mol.GetNumAtoms())
+    mol.AddConformer(conf)
+    conf = mol.GetConformer()
+    
+    atom_positions = {}
+    
+    for id, descriptor in enumerate(descriptors):
+        # print("Atom", id)
+        r, theta, phi, sign = descriptor
+
+        f = -1
+        c1 = -1
+        c2 = -1
+        
+        f = find_next_atom(id, id)
+        if f != -1:
+            c1 = find_next_atom(id, f)
+            if c1 != -1:
+                c2 = find_next_atom(id, c1, f)
+        
+        # print("smiles idx:", id, "f:", f, "c1:", c1, "c2:", c2)
+
+        pos = []
+        
+        if f != -1 and c1 != -1 and c2 != -1:
+            p_f = atom_positions.get(f)
+            p_c1 = atom_positions.get(c1)
+            p_c2 = atom_positions.get(c2)
+            
+            pos = calc_spherical_to_cartesian(r, theta, phi, sign, p_f, p_c1, p_c2)
+            
+
+            # if id == 4:
+            #     print(p_f)
+            #     print(p_c1)
+            #     print(p_c2)
+            #     print(pos)
+                # print(e1)
+                # print(e2)
+                # print(e3)
+                # print(actual_phi)
+                # print(local_relative_pos)
+                # print(np.linalg.norm(pos - p_f))
+            
+
+        elif f != -1 and c1 != -1:
+            if id != 2:
+                print(f"c2 was not found for atom {id}")
+
+            p_f = atom_positions.get(f)
+            p_c1 = atom_positions.get(c1)
+            if p_f is None or p_c1 is None:
+                print(f"Atom {id}: Reference atoms positions not fully defined. Assigning default position at origin.")
+                pos = np.array([0.0, 0.0, 0.0])
+            else:
+                v1 = p_c1 - p_f
+                e1 = v1 / np.linalg.norm(v1)
+                
+                # rng = [0.5,0.5,1]
+                rng = np.random.RandomState(42)
+                while True:
+                    arbitrary_vector = rng.rand(3) - 0.5  # Generate a vector with components between -0.5 and 0.5
+                    if np.linalg.norm(np.cross(e1, arbitrary_vector)) > 1e-6: # Ensure not collinear
+                        break # Exit loop if we found a non-collinear vector
+               
+                # Calculate e2 using cross product with the arbitrary vector, this will really be 
+                e2_dir = np.cross(e1, arbitrary_vector)
+                norm_e2_dir = np.linalg.norm(e2_dir)
+                if norm_e2_dir < 1e-6: # Vectors are collinear
+                    print("collinear")
+                    if np.allclose(arbitrary_vector, [1, 0, 0]):
+                        e2_dir = np.cross(e1, np.array([0, 1, 0]))
+                    elif np.allclose(arbitrary_vector, [0, 1, 0]):
+                        e2_dir = np.cross(e1, np.array([0, 0, 1]))
+                    else:
+                        e2_dir = np.cross(e1, np.array([1, 0, 0]))
+                    norm_e2_dir = np.linalg.norm(e2_dir)
+
+                e2 = e2_dir / norm_e2_dir # Normalize e2
+
+                e3 = np.cross(e1, e2)
+                e3 /= np.linalg.norm(e3)
+                
+                # Create rotation matrix
+                rotation_matrix = np.vstack([e1, e2, e3]).T  # 3x3 matrix
+                
+                # Convert spherical to Cartesian in local coordinates
+                actual_phi = sign * phi
+                local_relative_pos = relative(r, theta, actual_phi)
+                
+                # Rotate to global coordinates
+                global_relative_pos = rotation_matrix @ local_relative_pos
+                
+                # Assign global position
+                pos = p_f + global_relative_pos
+                
+        elif f != -1:
+            if id != 1:
+                print(f"c1 was not found for atom {id}")
+            p_f = atom_positions.get(f)
+            if p_f is None:
+                print(f"Atom {id}: Reference atom position not defined. Assigning default position at origin.")
+                pos = np.array([0.0, 0.0, 0.0])
+            else:
+                pos = p_f + np.array([r, 0.0, 0.0])
+
+        else:
+            if id != 0:
+                print(f"f was not found for atom {id}")
+            pos = np.array([0.0, 0.0, 0.0])
+
+        atom_positions[id] = pos
+
+        conf.SetAtomPosition(id, tuple(pos))
+        # print(f"Coords: {pos}")
+
+    return mol
+
+def parse_molecule_with_spherical_coordinates(embedded_smiles):
+    smiles, descriptors = parse_embedded_smiles(embedded_smiles)
+    mol = Chem.MolFromSmiles(smiles, sanitize=False)
+    if exclude_h:
+        mol = Chem.RemoveHs(mol)
+        mol = Chem.SanitizeMol(mol)
+    if mol is None: 
+        raise ValueError(f"Invalid SMILES '{smiles}'")
+    return assign_cartesian_coordinates(mol, descriptors)
+
+
+def reconstruct_sdf_from_embedded_smiles(embedded_smiles_list, output_sdf):
+    """Reconstructs molecules from embedded SMILES and writes to an SDF file."""
+    writer = Chem.SDWriter(output_sdf)
+    if writer is None:
+        raise ValueError(f"Could not create SDF writer for {output_sdf}")
+    for idx, embedded_smiles in enumerate(embedded_smiles_list):
+        if idx == 200000:
+            break
+        try:
+            if idx % 5000 == 0:
+                print(idx)
+                sys.stdout.flush()
+
+            smiles, descriptors = parse_embedded_smiles(embedded_smiles)
+            # print(f"\nProcessing Molecule {idx}...")
+            # print(f"SMILES: {smiles}")
+            # print(f"Descriptors: {descriptors}")
+
+            mol = Chem.MolFromSmiles(smiles, sanitize=False)
+            if exclude_h:
+                mol = Chem.RemoveHs(mol)
+                mol = Chem.SanitizeMol(mol)
+            if mol is None:
+                print(f"Molecule {idx+1}: Invalid SMILES '{smiles}'.")
+                continue
+
+            mol_with_coords = assign_cartesian_coordinates(mol, descriptors)
+            
+            # Optional: Optimize geometry (e.g., using UFF)
+            # AllChem.UFFOptimizeMolecule(mol_with_coords)
+            
+            writer.write(mol_with_coords)
+            # print(f"Molecule {idx}: Successfully reconstructed and written to SDF.")
+        
+        except Exception as e:
+            print(f"Molecule {idx+1}: Error - {str(e)}")
+    
+    writer.close()
+    print(f"\nAll molecules have been written to '{output_sdf}'")
+
+out_file = "output_inv_with_H.txt"
+if exclude_h:
+    out_file = "output_inv.txt"
+sys.stdout = open(out_file, "w") 
+
+data_file = "/auto/home/filya/3DMolGen/molgen3D/data_processing/mol_9496_.jsonl"
+# data_file = "/auto/home/filya/3DMolGen/train_embedded_spherical_with_H/train_data_0.jsonl"
+# if exclude_h:
+#     data_file = "/auto/home/filya/3DMolGen/train_embedded_spherical/train_data_0.jsonl"
+
+embedded_smiles_list = []
+with open(data_file, 'r') as f:
+    for line_number, line in enumerate(f):
+        try:
+            json_object = json.loads(line)
+            if "conformers" in json_object:
+                conformers_data = json_object["conformers"]
+                if isinstance(conformers_data, dict) and "embedded_smiles" in conformers_data:
+                    embedded_smiles_list.append(conformers_data["embedded_smiles"])
+                else:
+                    print(f"Warning: 'embedded_smiles' key not found within 'conformers' on line {line_number} or 'conformers' is not in expected format.")
+            else:
+                print(f"Warning: 'conformers' key not found in JSON object on line {line_number}.")
+
+        except json.JSONDecodeError as e:
+            print(f"Error: JSONDecodeError on line {line_number}: {e}")
+            print(f"Line causing error: {line.strip()}")
+
+output_sdf = "mol_9496-rec.sdf"
+# output_sdf = "reconstructed_mols/reconstructed_molecules_1_with_H.sdf"
+# if exclude_h:
+#     output_sdf = "reconstructed_mols/reconstructed_molecules_1.sdf"
+#     output_sdf = "reconstructed_mols/reconstructed_molecules_1.sdf"
+reconstruct_sdf_from_embedded_smiles(embedded_smiles_list, output_sdf)
---
+++ molgen3D/data_processing/get_spherical_from_cartesian.py
@@ -0,0 +1,373 @@
+import numpy as np
+from rdkit import Chem
+import ast
+from scipy.spatial import distance
+import sys
+import json
+import os
+from ogb.lsc import PCQM4Mv2Dataset
+from loguru import logger as log
+
+exclude_h = False
+
+def is_collinear(p1, p2, p3, tolerance=1e-6):
+    v1 = p2 - p1
+    v2 = p3 - p1
+    cross_product = np.cross(v1, v2)
+    return np.allclose(cross_product, [0, 0, 0], atol=tolerance)
+
+def get_new_atom(mol, atom_order):
+    if not atom_order:
+        return -1, "??"
+    original_index = atom_order.pop(0)
+    atom = mol.GetAtomWithIdx(original_index)
+    if not atom:
+        raise ValueError("Coundn't find the atom by the given index")
+    # if exclude_h:
+    #     while atom.GetSymbol() == "H" and atom_order:
+    #         original_index = atom_order.pop(0)
+    #         atom = mol.GetAtomWithIdx(original_index)
+    #     if atom.GetSymbol() == "H":
+    #         return -1, "??"
+    return original_index, atom.GetSymbol()
+
+def get_smiles(mol):
+    smiles_list = []
+    canonical_smiles = Chem.MolToSmiles(mol, canonical=True, isomericSmiles=False)
+    # print("smiles", canonical_smiles)
+
+    atom_order = ast.literal_eval(mol.GetProp('_smilesAtomOutputOrder'))
+
+    original_index, atom_name = get_new_atom(mol, atom_order)
+    
+    i = 0
+    while i < len(canonical_smiles):
+        if canonical_smiles[i:i+len(atom_name)].upper() == atom_name.upper():
+            smiles_list.append((atom_name, original_index))
+            i += len(atom_name)
+            original_index, atom_name = get_new_atom(mol, atom_order)
+        else:
+            i += 1
+
+    smiles_to_sdf = {}
+    sdf_to_smiles = {}
+    for i, (ch, id) in enumerate(smiles_list):
+        smiles_to_sdf[i] = id
+        sdf_to_smiles[id] = i
+    # print("sdf_to_smiles ", sdf_to_smiles)
+    # print("smiles_to_sdf ", smiles_to_sdf)
+    # sys.stdout.flush()
+
+    return smiles_list, smiles_to_sdf, sdf_to_smiles
+
+def get_ans(mol, descriptors):
+    ans = ""
+
+    canonical_smiles = Chem.MolToSmiles(mol, canonical=True, isomericSmiles=False)
+    atom_order = ast.literal_eval(mol.GetProp('_smilesAtomOutputOrder'))
+    original_index, atom_name = get_new_atom(mol, atom_order)
+
+    smiles_id = 0
+    i = 0
+    while i < len(canonical_smiles):
+        if canonical_smiles[i:i+len(atom_name)].upper() == atom_name.upper():
+            ans += canonical_smiles[i:i+len(atom_name)]
+            ans += descriptors[smiles_id]
+            smiles_id += 1
+            i += len(atom_name)
+            original_index, atom_name = get_new_atom(mol, atom_order)
+        else:
+            ans += canonical_smiles[i]
+            i += 1
+    return ans
+
+def calculate_spherical_from_cartesian(current_atom_coord, focal_atom_coord, c1_atom_coord, c2_atom_coord):
+    r = distance.euclidean(current_atom_coord, focal_atom_coord)
+
+    v_cf = c1_atom_coord - focal_atom_coord
+    v_c2f = c2_atom_coord - focal_atom_coord
+    v_if = current_atom_coord - focal_atom_coord
+
+    # Calculate normal vector to the plane defined by focal and reference atoms
+    normal_vector = np.cross(v_cf, v_c2f)
+    normal_vector_unit = normal_vector / np.linalg.norm(normal_vector) # norm_normal_vector is not 0, bcs f,c1,c2 are not collinear
+
+    # Calculate theta (polar angle)
+    cos_theta = np.dot(v_if, normal_vector_unit) / r # r is norm_v_if
+    if(cos_theta > 1.0 or cos_theta < -1.0):
+        log.error("cos_theta is not correct: ", cos_theta)
+        cos_theta = np.clip(cos_theta, -1.0, 1.0)
+    theta = np.arccos(cos_theta)
+
+    # Calculate phi (azimuthal angle)
+    proj_if = v_if - r * cos_theta * normal_vector_unit
+    norm_proj_if = np.linalg.norm(proj_if)
+    if norm_proj_if < 1e-7: #theta is 0, phi ?
+        phi = 0
+    else:
+        proj_if /= norm_proj_if
+        v_cf /= np.linalg.norm(v_cf) # norm_v_cf is not 0, bcs cur c1 is not f
+        
+        cos_phi = np.dot(proj_if, v_cf) 
+        if(cos_phi > 1.0 or cos_phi < -1.0):
+            log.error("cos_phi is not correct: ", cos_phi)
+            cos_phi = np.clip(cos_phi, -1.0, 1.0)
+        phi = np.arccos(cos_phi)
+
+        cross_proj_cf = np.cross(v_cf, proj_if)
+        if np.dot(normal_vector, cross_proj_cf) < 0: # cospi = -1, cos0 = 1
+            phi = -phi
+
+    return r, theta, abs(phi), np.sign(phi)
+
+def calculate_descriptors(mol, mol_id, smiles_index, sdf_to_smiles, smiles_to_sdf, coords):
+    """Calculates generation descriptors for a specific atom in a molecule."""
+    def find_next_atom(begin_sdf_id, sdf_id1, sdf_id2 = None):
+        begin_smiles_id = sdf_to_smiles[begin_sdf_id]
+        smiles_id1 = sdf_to_smiles[sdf_id1]
+        this_atom1 = mol.GetAtomWithIdx(sdf_id1)
+        neighbors1 = [neighbor.GetIdx() for neighbor in this_atom1.GetNeighbors()]
+        neighbors2 = []
+        smiles_id2 = -1
+        if sdf_id2 is not None:
+            smiles_id2 = sdf_to_smiles[sdf_id2]
+            this_atom2 = mol.GetAtomWithIdx(sdf_id2)
+            neighbors2 = [neighbor.GetIdx() for neighbor in this_atom2.GetNeighbors()]
+        for i in range(begin_smiles_id - 1, -1, -1):
+            if i == smiles_id1 or i == smiles_id2:
+                continue
+            if smiles_to_sdf[i] in neighbors1 or smiles_to_sdf[i] in neighbors2:
+                return smiles_to_sdf[i]
+        return -1
+    
+    def find_ref_points(atom_index):
+        f = -1
+        c1 = -1
+        c2 = -1
+        focal_atom_coord = -1
+        c1_atom_coord = -1
+        c2_atom_coord = -1
+
+        f = find_next_atom(atom_index, atom_index)
+        if f != -1:
+            focal_atom_coord = coords[f]
+            c1 = find_next_atom(atom_index, f)
+            if c1 != -1:
+                c1_atom_coord = coords[c1]
+                c2 = find_next_atom(atom_index, c1, f)
+                if c2 != -1:
+                    c2_atom_coord = coords[c2]
+        return f, c1, c2, focal_atom_coord, c1_atom_coord, c2_atom_coord
+
+
+    atom_index = smiles_to_sdf[smiles_index]
+    f, c1, c2, focal_atom_coord, c1_atom_coord, c2_atom_coord = find_ref_points(atom_index)
+
+    # print("atom idx:", atom_index, "smiles idx:", smiles_index, "f:", sdf_to_smiles.get(f, -1), "c1:", sdf_to_smiles.get(c1, -1), "c2:", sdf_to_smiles.get(c2, -1))
+
+    # Calculate spherical coordinates
+    if f == -1: 
+        if smiles_index != 0:
+            log.error(f"f was not found for mol {mol_id}, atom {atom_index}, {sdf_to_smiles[atom_index]}")
+            return np.array([-1])
+        return np.array([0, np.pi / 2, 0, np.sign(0)])
+    if c1 == -1:
+        if smiles_index != 1:
+            log.error(f"c1 was not found for atom {atom_index}, {sdf_to_smiles[atom_index]}")
+        return np.array([distance.euclidean(coords[atom_index], focal_atom_coord), np.pi / 2, 0, np.sign(0)])
+    if c2 == -1:
+        # print("f-smiles:", sdf_to_smiles[f])
+        # print("c1-smiles:", sdf_to_smiles[c1])
+        if smiles_index != 2:
+            log.error(f"c2 was not found for atom {atom_index}, {sdf_to_smiles[atom_index]}")
+        #assume that the point is on XY plane
+        if is_collinear(focal_atom_coord, c1_atom_coord, coords[atom_index]):
+            log.error("f,c1,i are collinear in atom", atom_index)
+            return np.array([-1])
+            # return np.array([distance.euclidean(coords[atom_index], focal_atom_coord), np.pi / 2, 0, np.sign(0)])
+
+        proj_if = coords[atom_index] - focal_atom_coord #v_if=proj_if
+        v_cf = c1_atom_coord - focal_atom_coord
+        norm_proj_if = np.linalg.norm(proj_if)
+        norm_v_cf = np.linalg.norm(v_cf)
+        if norm_proj_if > 1e-6 and norm_v_cf > 1e-6:
+            cos_phi = np.dot(proj_if, v_cf) / (norm_proj_if * norm_v_cf)
+            if(cos_phi > 1.0 or cos_phi < -1.0):
+                log.error("cos_phi is not correct: ", cos_phi)
+                cos_phi = np.clip(cos_phi, -1.0, 1.0)
+            phi = np.arccos(cos_phi)
+
+            normal_vector = np.cross(v_cf, proj_if)
+            norm_normal = np.linalg.norm(normal_vector)
+            
+            if norm_normal > 1e-8:
+                normal_vector /= norm_normal  # Normalize the normal vector
+            else:
+                log.error("Can't define a valid normal vector (vectors are collinear).")
+                return np.array([-1])
+            
+            # Determine the sign of phi based on the direction of the normal vector
+            cross_proj_cf = np.cross(v_cf, proj_if)
+            if np.dot(normal_vector, cross_proj_cf) < 0:
+                phi = -phi
+        else:
+            log.error("can't divide by 0")
+            return np.array([-1])
+        return np.array([distance.euclidean(coords[atom_index], focal_atom_coord), np.pi / 2, abs(phi), np.sign(phi)])
+    
+    # Here we have f,c1,c2
+    if is_collinear(focal_atom_coord, c1_atom_coord, c2_atom_coord):
+        log.error("f,c1,c2 are colinear", atom_index)
+        return np.array([-1])
+    
+    # Calculate spherical coordinates
+    current_atom_coord = coords[atom_index]
+
+    r, theta, phi, sign_phi = calculate_spherical_from_cartesian(current_atom_coord, focal_atom_coord, c1_atom_coord, c2_atom_coord)
+
+
+    # if atom_index == 6:
+    #     print("current_atom_coord: ", current_atom_coord, " focal_atom_coord: ", focal_atom_coord, "c1_atom_coord: ", c1_atom_coord, "c2_atom_coord: ", c2_atom_coord)
+        # print("v_cf: ", np.linalg.norm(v_cf))
+        # print("v_c2f: ", np.linalg.norm(v_c2f))
+        # print("v_if:", v_if)
+        # print("r: ", r)
+        # print("cos_theta", cos_theta)
+        # print("theta", theta)
+        # print("proj_if", proj_if)
+        # print("norm_proj_if", norm_proj_if)
+        # print("norm_v_cf", norm_v_cf)
+        # print(phi)
+    
+    # e1 = v_cf / np.linalg.norm(v_cf)
+    # e2 = np.cross(normal_vector_unit, e1)
+    # proj_if = v_if - (np.dot(v_if, normal_vector_unit)) * normal_vector_unit
+    # phi = np.arctan2(np.dot(proj_if, e2), np.dot(proj_if, e1))
+            
+    return np.array([r, theta, phi, sign_phi])
+
+    
+        
+def get_json(mol, embedded, sample):
+    return {"canonical_smiles": Chem.MolToSmiles(mol, canonical=True, isomericSmiles=False), 
+            "pcqm4v2_smiles": sample[0], 
+            "pcqm4v2_label": sample[1], 
+            "conformers": {"embedded_smiles": embedded}}
+    
+
+def get_mol_descriptors(mol, mol_id, precision=4):
+    conformer = mol.GetConformer()
+    coords = conformer.GetPositions()
+    smiles, smiles_to_sdf, sdf_to_smiles = get_smiles(mol) 
+    all_descriptors = []
+    for i in range(len(smiles)):
+        descriptors = calculate_descriptors(mol, mol_id, i, sdf_to_smiles, smiles_to_sdf, coords)
+        if descriptors.size and descriptors[0] != -1:
+            desc_str = ",".join(f"{val:.{precision}f}" for val in descriptors)
+            desc_str = desc_str[:-5] # give the sign as an integer
+            all_descriptors.append(f"<{desc_str}>")
+        else:
+            return "no descriptors"
+    
+    return all_descriptors
+
+def embed_coordinates_spherical(mol, smiles, order, precision=4):
+    all_descriptors = get_mol_descriptors(mol, 0, precision)
+    return get_ans(mol, all_descriptors)
+
+def process_and_find_descriptors(sdf, val_indices):
+    # supplier = Chem.SDMolSupplier(sdf, sanitize=exclude_h, removeHs=exclude_h)
+    supplier = Chem.SDMolSupplier("mol_9496-rec.sdf", sanitize=exclude_h, removeHs=exclude_h)
+    dataset = PCQM4Mv2Dataset(root = '/auto/home/menuab', only_smiles = True)
+
+    train_data = []
+    val_data = []
+    for i, mol in enumerate(supplier): #3378606
+        # if i < 9496:
+        #     continue
+        # if i > 9496:
+        #     break
+        # writer = Chem.SDWriter("mol_9496-gen.sdf")
+        # writer.write(mol)
+        # writer.close()
+        # if i == 200000:
+        #     break
+        if i % 1000 == 0:
+            print(f"Mol {i}...")
+            sys.stdout.flush()
+        if not mol:
+            log.error(f"Failed to process mol {i}")
+            return
+        else:
+            all_descriptors = get_mol_descriptors(mol, i)
+            descriptors = get_ans(mol, all_descriptors)
+            if descriptors != "no descriptors":
+                json_string = get_json(mol, descriptors, dataset[i])
+                
+                if i in val_indices:
+                    val_data.append(json_string)
+                else:
+                    train_data.append(json_string)
+            else:
+                log.error(f"Excluding mol {i}")
+
+    current_file = None
+    data_item = train_data[0]
+    current_file = open(f"mol_9496_.jsonl", 'w')
+    json.dump(data_item, current_file)
+    current_file.write('\n')
+    current_file.close()
+
+
+    # train_folder = "train_embedded_spherical_with_H"
+    # if exclude_h:
+    #     train_folder = "train_embedded_spherical"
+    # os.makedirs(train_folder, exist_ok=True)
+
+    # file_counter = 0
+    # current_file = None
+    # new_file_freq = 1000000
+
+    # for i, data_item in enumerate(train_data):
+    #     if i % new_file_freq == 0:
+    #         if current_file:
+    #             current_file.close()
+    #         current_file = open(os.path.join(train_folder, f"train_data_{file_counter}.jsonl"), 'w')
+    #         file_counter += 1
+
+    #     if current_file:
+    #         json.dump(data_item, current_file)
+    #         current_file.write('\n')
+
+    # if current_file:
+    #     current_file.close()
+
+    # valid_folder = "valid_embedded_spherical_with_H"
+    # if exclude_h:
+    #     valid_folder = "valid_embedded_spherical"
+    # os.makedirs(valid_folder, exist_ok=True)
+    # with open(os.path.join(valid_folder, "valid_data.jsonl"), "w") as file:
+    #     for d in val_data:
+    #         json.dump(d, file)
+    #         file.write("\n")
+    #     file.close()
+
+if __name__ == '__main__':
+    out_file = "output_gen_with_H.txt"
+    if exclude_h:
+        out_file = "output_gen.txt"
+    sys.stdout = open(out_file, "w") 
+    sdf_file = '/auto/home/menuab/pcqm4m-v2-train.sdf'
+    val_indices_file = "/auto/home/menuab/code/3DMolGen/data/pcqm/pcqm4v2_valid_indice.txt"
+    val_indices = []
+    with open(val_indices_file, 'r') as f:
+        for line in f:
+            try:
+                index = int(line.strip())
+                val_indices.append(index)
+            except ValueError:
+                log.error(f"Skipping invalid line: {line.strip()}")
+    try:
+        process_and_find_descriptors(sdf_file, val_indices)
+    except ValueError as e:
+        log.error(f"Error: {e}")
