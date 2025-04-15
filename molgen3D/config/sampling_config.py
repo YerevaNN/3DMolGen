ad06cbe68b59b6a844e759eae9805d1097ac10ab
MenuaB <menooa.vold@gmail.com>
2025-04-15 20:49:28 +0400

    move some args to inference code

--- molgen3D/config/sampling_config.py
+++ molgen3D/config/sampling_config.py
@@ -2,22 +2,16 @@ from transformers import GenerationConfig
 
 greedy_config = GenerationConfig(
     do_sample=False,
-    max_new_tokens=3000,
-    eos_token_id=128329,
 )
 
 beam_search_config = GenerationConfig(
     num_beams=4,  
     num_beam_groups=1,
     diversity_penalty=0,
-    max_new_tokens=3000,
-    eos_token_id=128329,
 )
 
 top_p_sampling_config = GenerationConfig (
   do_sample=True,
-  eos_token_id=128329,
-  max_new_tokens=3000,
   temperature=0.8,
   top_p=0.9
 )
@@ -25,8 +19,6 @@ top_p_sampling_config = GenerationConfig (
 min_p_sampling_config= GenerationConfig(
     do_sample=True,
     min_p = 0.05,
-    max_new_tokens=3000,
-    eos_token_id=128329,
 )
 
 sampling_configs = {
