860c73806d505421045c20cf039c5ef2de42af62
MenuaB <menooa.vold@gmail.com>
2025-05-19 12:33:07 +0400

    update paths and sampling configs

--- molgen3D/config/sampling_config.py
+++ molgen3D/config/sampling_config.py
@@ -10,22 +10,51 @@ beam_search_config = GenerationConfig(
     diversity_penalty=0,
 )
 
-top_p_sampling_config = GenerationConfig (
+top_p_sampling_config1 = GenerationConfig (
   do_sample=True,
   temperature=1.0,
   top_p=0.8
 )
 
-min_p_sampling_config= GenerationConfig(
+top_p_sampling_config2 = GenerationConfig (
+  do_sample=True,
+  temperature=1.2,
+  top_p=0.8
+)
+
+top_p_sampling_config3 = GenerationConfig (
+  do_sample=True,
+  temperature=1.0,
+  top_p=0.6
+)
+
+min_p_sampling_config1= GenerationConfig(
     do_sample=True,
+    temperature=1.0,
     min_p = 0.05,
+)
+
+min_p_sampling_config2= GenerationConfig(
+    do_sample=True,
+    temperature=1.2,
+    min_p = 0.05,
+)
+
+min_p_sampling_config3= GenerationConfig(
+    do_sample=True,
+    temperature=1.0,
+    min_p = 0.1,
 )
 
 sampling_configs = {
     "greedy": greedy_config,
     "beam_search": beam_search_config,
-    "top_p_sampling": top_p_sampling_config,
-    "min_p_sampling": min_p_sampling_config,
+    "top_p_sampling1": top_p_sampling_config1,
+    "top_p_sampling2": top_p_sampling_config2,
+    "top_p_sampling3": top_p_sampling_config3,
+    "min_p_sampling1": min_p_sampling_config1,
+    "min_p_sampling2": min_p_sampling_config2,
+    "min_p_sampling3": min_p_sampling_config3,
 }
 
 gen_num_codes = {
