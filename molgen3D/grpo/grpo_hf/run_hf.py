9be80e506329d461b58cc1b9534da64032a6a3e6
MenuaB <menooa.vold@gmail.com>
2025-06-10 13:38:18 +0400

    add config file

--- molgen3D/grpo/grpo_hf/run_hf.py
+++ molgen3D/grpo/grpo_hf/run_hf.py
@@ -1,10 +1,13 @@
-from typing import List
+from typing import List, Dict, Any
 import argparse
 from collections import namedtuple
 import os
 from pathlib import Path
 from ast import literal_eval
 import yaml
+import submitit
+from dataclasses import dataclass, field
+from typing import Optional
 
 import torch
 import torch.nn.functional as F
@@ -12,57 +15,54 @@ import numpy as np
 
 from transformers import AutoTokenizer, AutoModelForCausalLM,  PreTrainedTokenizerBase
 from datasets import Dataset, Features, Value
-from trl import GRPOConfig
+from trl import GRPOConfig as TRLGRPOConfig
 from molgen3D.grpo.grpo_hf.grpo_trainer import GRPOTrainer
 # from utils.mol import find_valid_mols
 
 from loguru import logger
 import re
 from molgen3D.evaluation.utils import extract_between
+from molgen3D.grpo.grpo_hf.config import Config
 
 
-def search(args):
+def main(config: Config, enable_wandb: bool = False):
     logger.info(f"Running GRPO")
     device = "cuda:0"
 
-    training_args = GRPOConfig(
-        output_dir=args.grpo.output_dir,
+    training_args = TRLGRPOConfig(
+        output_dir=config.grpo.output_dir,
         save_strategy="no",
-        max_completion_length=args.generation.max_completion_length,
-        # eos_token_id=args.processing.eos_token_id,
-        learning_rate=args.grpo.learning_rate,
-        lr_scheduler_type=args.grpo.scheduler,
-        adam_beta1=args.grpo.adam_beta1,
-        adam_beta2=args.grpo.adam_beta2,
-        weight_decay=args.grpo.weight_decay,
-        warmup_ratio=args.grpo.warmup_ratio,
-        max_grad_norm=args.grpo.max_grad_norm,
-        temperature=args.grpo.temperature,
-        num_train_epochs=args.grpo.num_epochs,
-        num_generations=args.grpo.num_generations,
-        beta=args.grpo.beta,
-        per_device_train_batch_size=args.grpo.batch_size,
-        gradient_accumulation_steps=args.grpo.grad_acc_steps,
+        max_completion_length=config.generation.max_completion_length,
+        learning_rate=config.grpo.learning_rate,
+        lr_scheduler_type=config.grpo.scheduler,
+        adam_beta1=config.grpo.adam_beta1,
+        adam_beta2=config.grpo.adam_beta2,
+        weight_decay=config.grpo.weight_decay,
+        warmup_ratio=config.grpo.warmup_ratio,
+        max_grad_norm=config.grpo.max_grad_norm,
+        temperature=config.grpo.temperature,
+        num_train_epochs=config.grpo.num_epochs,
+        num_generations=config.grpo.num_generations,
+        beta=config.grpo.beta,
+        per_device_train_batch_size=config.grpo.batch_size,
+        gradient_accumulation_steps=config.grpo.grad_acc_steps,
         log_on_each_node=False,
         bf16=True,
-        report_to="wandb",
-        run_name="my_grpo_experiment",
+        report_to="wandb" if enable_wandb else "none",
+        run_name=config.run.name,
         logging_steps=1,
     )
 
     model = AutoModelForCausalLM.from_pretrained(
-        args.model.checkpoint_path,
+        config.model.checkpoint_path,
         torch_dtype=torch.bfloat16,
         device_map=None
-        # attn_implementation="sdpa"
     ).to(device)
     tokenizer = AutoTokenizer.from_pretrained(
-        args.model.tokenizer_path,
-        # padding_side="left"
+        config.model.tokenizer_path,
     )
 
-
-    dataset = Dataset.from_csv(args.dataset.dataset_path)
+    dataset = Dataset.from_csv(config.dataset.dataset_path)
 
     def reward_function(completions, **kwargs):
         tag_pattern = re.compile(r'<[^>]*>')
@@ -80,9 +80,8 @@ def search(args):
         print('end of batch ----------******************----------------------')
         return rewards
 
-
-    tokenizer.pad_token_id = tokenizer.get_vocab()[args.model.pad_token]
-    mol_end_token_id = tokenizer.get_vocab()[args.model.mol_tags[1]]
+    tokenizer.pad_token_id = tokenizer.get_vocab()[config.model.pad_token]
+    mol_end_token_id = tokenizer.get_vocab()[config.model.mol_tags[1]]
 
     trainer = GRPOTrainer(
         model=model,
@@ -92,23 +91,35 @@ def search(args):
         ],
         args=training_args,
         train_dataset=dataset,
-        # mol_end_token_id=mol_end_token_id
     )
     trainer.train()
 
 
-class ExperimentArgs:
-    pass
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--config", required=True, type=str)
+    parser.add_argument("--wandb", action="store_true", help="Enable wandb tracking")
     cmd_args = parser.parse_args()
-    args_dict = yaml.safe_load(open(cmd_args.config, "r"))
-    # print(args_dict)
-    args = ExperimentArgs()
+    
+    # Load configuration
+    config = Config.from_yaml(cmd_args.config)
 
-    # add the arguments to the args
-    for k, v in args_dict.items():
-        setattr(args, k, namedtuple(k.title(), v.keys())(**v))
-    search(args)
+    # Setup submitit executor
+    executor = submitit.AutoExecutor(folder="~/slurm_jobs/grpo/job_%j")
+    node = "a100"
+    n_gpus = 1
+    
+    executor.update_parameters(
+        name=config.run.name,
+        timeout_min=24 * 24 * 60,
+        gpus_per_node=n_gpus,
+        nodes=1,
+        mem_gb=80,
+        cpus_per_task=n_gpus * 8,
+        slurm_additional_parameters={"partition": node},
+    )
+
+    # Submit job to SLURM
+    job = executor.submit(main, config=config, enable_wandb=cmd_args.wandb)
+
+    # main(config=config, enable_wandb=cmd_args.wandb)  # Run the main function immediately for local testing
