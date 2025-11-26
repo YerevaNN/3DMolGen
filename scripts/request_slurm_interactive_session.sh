0a45d6a56692b5ad16f90904d3409c63d5980775
MenuaB <menooa.vold@gmail.com>
2025-11-26 17:32:55 +0400

    update book session

--- scripts/request_slurm_interactive_session.sh
+++ scripts/request_slurm_interactive_session.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
+#SBATCH --job-name=debugging
+#SBATCH --cpus-per-task=16
+#SBATCH --partition=h100
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:4
+#SBATCH --mem=32G
+#SBATCH --time=02:00:00
+
+
 srun \
   --job-name=debugging \
   --partition=a100 \
